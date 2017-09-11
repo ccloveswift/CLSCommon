2657be8a1dc24922311e11eca19a1a0715363a68
fhr <fenghaoran@camera360.com>
2017-09-11 20:06:02 +0800

    Cc 更新

---
+++ Classes/UI/class_UIButton.swift
@@ -0,0 +1,41 @@
+//
+//  class_UIButton.swift
+//  Pods
+//
+//  Created by Cc on 2017/9/9.
+//
+//
+
+import UIKit
+
+public class class_UIButton: UIButton {
+    
+    public typealias tClickBlock = ((_ sender: class_UIButton) -> Void)?
+    
+    public var mClickBlock: tClickBlock
+    
+    public override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+        self.addTarget(self, action: #selector(onClick(sender:)), for: .touchUpInside)
+    }
+    
+    deinit {
+        
+        self.removeTarget(self, action: nil, for: .allTouchEvents)
+        CLSLogInfo("deinit class_UIButton")
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func onClick(sender: class_UIButton) {
+        
+        if let block = mClickBlock {
+            
+            block(sender)
+        }
+    }
+}
